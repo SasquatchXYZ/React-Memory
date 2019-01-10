8bfa44596b5bb2cd7dd301a9747eb9ea636bd780
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-10 01:53:23 -0500

    changed images to possibly temporary other ones

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,7 +3,7 @@ import './Title.css';
 
 const TitleBar = props => (
   <div className="title-bar">
-    <h1 className="title">React Memory Game</h1>
+    <h1 className="title">Gundam Memory Game</h1>
     <div className="board">
       <h2 className="message-board">{props.message}</h2>
       <h2 className="scoreboard">Score: {props.score}</h2>
