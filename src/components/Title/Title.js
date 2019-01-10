273f946ce9c58a148384e159afdb18eb241b2843
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-10 00:02:06 -0500

    Working with styling

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -4,8 +4,10 @@ import './Title.css';
 const TitleBar = props => (
   <div className="title-bar">
     <h1 className="title">React Memory</h1>
-    <p className="scoreboard">{props.score}</p>
-    <p className="message-board">{props.message}</p>
+    <div className="board">
+      <h1 className="message-board">{props.message}</h1>
+      <h1 className="scoreboard">{props.score}</h1>
+    </div>
   </div>
 );
 
