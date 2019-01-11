8d217db6a58113652697531198eb3242a285bc57
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-11 00:11:49 -0500

    updated Readme and added screenshot

--- src/components/GameCard/GameCard.js
+++ src/components/GameCard/GameCard.js
@@ -6,22 +6,6 @@ const GameCard = props => (
     <div className="img-container">
       <img alt={props.name} src={props.image}/>
     </div>
-    {/*<div className="content">
-      <ul>
-        <li>
-          <strong>Name:</strong> {props.name}
-        </li>
-        <li>
-          <strong>Occupation:</strong> {props.occupation}
-        </li>
-        <li>
-          <strong>Location:</strong> {props.location}
-        </li>
-      </ul>
-    </div>
-    <span onClick={() => props.removeCard(props.id)} className="remove">
-      x
-    </span>*/}
   </div>
 );
 
