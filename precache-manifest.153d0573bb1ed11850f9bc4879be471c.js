54d95b40e38609c059647e9dcbbb61e9c82814c1
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-01-11 00:42:37 -0500

    Updates

---
+++ precache-manifest.153d0573bb1ed11850f9bc4879be471c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6633a16d942ac9d7e1a4ad258417fc8d",
+    "url": "/React-Memory/static/media/what-the-hex.6633a16d.png"
+  },
+  {
+    "revision": "00dc0de315c0dff17dbcf84adbcf60c8",
+    "url": "/React-Memory/static/media/what-the-hex-dark.00dc0de3.png"
+  },
+  {
+    "revision": "6816fe28d65a6fdf0802",
+    "url": "/React-Memory/static/js/runtime~main.6816fe28.js"
+  },
+  {
+    "revision": "558386c4b39ce49a7fed",
+    "url": "/React-Memory/static/js/main.558386c4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/React-Memory/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "558386c4b39ce49a7fed",
+    "url": "/React-Memory/static/css/main.49052a12.chunk.css"
+  },
+  {
+    "revision": "e3e27c00a802c9cc9cf396458311db18",
+    "url": "/React-Memory/index.html"
+  }
+];
